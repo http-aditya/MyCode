5dd12fefd85242d2cc0f39f821dd635706d6a427
http:aditya <150951647+http-aditya@users.noreply.github.com>
2023-12-21 02:56:00 +0530

    Add files via upload

---
+++ youtube/src/quiz/Main.java
@@ -0,0 +1,76 @@
+package quiz;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+	 public static void main(String[] args) {
+	        Quiz q=new Quiz();
+	        q.logic();
+
+	    }
+	}
+
+
+	class Quiz {
+	    Scanner sc = new Scanner(System.in);
+	    int correctAnsCount = 0;
+	    int wrongAnsCount = 0;
+
+	    public void logic() {
+	        Question q1 = new Question(" Which statement is true about Java?", "A. Java is a sequence-dependent programming language ", "B. Java is a code dependent programming language ", "C. Java is a platform-dependent programming language ", "D. Java is a platform-independent programming language ");
+	        Question q2 = new Question("What is the extension of java code files?", " A. .txt", "B. .pdf", "C. .sql", "D. .java");
+	        Question q3 = new Question("Who invented Java Programming? ", "A. Guido van Rossum", "B. James Gosling", "C. Dennis Ritchie", "D. Bjarne Stroustrup");
+	        Question q4 = new Question("Which one of the following is not a Java feature?", "A. Object-oriented", "B. Use of pointers", "C. Portable", "D. Dynamic and Extensible");
+	        Question q5 = new Question("Which of these cannot be used for a variable name in Java?", "A. identifier & keyword", "B. identifier", "C. Keyword", "D. none of the mentioned");
+
+	        Map<Question, Character> hmap = new HashMap<>();
+	        hmap.put(q1, 'D');
+	        hmap.put(q1, 'd');
+	        hmap.put(q2, 'D');
+	        hmap.put(q2, 'd');
+	        hmap.put(q3, 'B');
+	        hmap.put(q3, 'b');
+	        hmap.put(q4, 'B');
+	        hmap.put(q4, 'b');
+	        hmap.put(q5, 'C');
+	        hmap.put(q5, 'c');
+
+	        for (Map.Entry<Question, Character> map : hmap.entrySet()) {
+	            System.out.println(map.getKey().getQuestion());
+	            System.out.println(map.getKey().getOption1());
+	            System.out.println(map.getKey().getOption2());
+	            System.out.println(map.getKey().getOption3());
+	            System.out.println(map.getKey().getOption4());
+
+	            System.out.println("Enter Your Answer: ");
+	            Character ans = sc.next().charAt(0);
+
+	            int cans = Character.compare(ans, map.getValue());
+	            if (cans == 0) {
+	                System.out.println("Correct");
+	                correctAnsCount++;
+	            } else {
+	                System.out.println("Wrong");
+	                wrongAnsCount++;
+	            }
+
+	        }
+
+	        System.out.println();
+	        System.out.println("--------Result---------");
+	        System.out.println("Total Questions: " + hmap.size());
+	        System.out.println("Correct Answered Questions : " + correctAnsCount);
+	        System.out.println("Wrong Answered Questions : " + wrongAnsCount);
+	        int percentage = (100 * correctAnsCount) / hmap.size();
+	        System.out.println("Percentage : " + percentage);
+	        if (percentage > 95) {
+	            System.out.println("Performance : Very Good");
+	        } else if (percentage < 35) {
+	            System.out.println("Performance : Very Low");
+	        } else {
+	            System.out.println("Performance : Medium");
+	        }
+}
+}
